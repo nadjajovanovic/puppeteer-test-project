c98924eef3009dca63ac37c95edb84e85cc24141
Nadja <j.nadja97@gmail.com>
2024-03-20 00:51:24 +0100

    First tasks finished

--- src/utils/consts.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.path = exports.fs = exports.paginationUrl = exports.baseUrl = exports.url = void 0;
-exports.url = require("../config/config.json");
-exports.baseUrl = exports.url.productUrl;
-exports.paginationUrl = `${exports.baseUrl}?ref=pagination&page=1`;
-exports.fs = require("fs");
-exports.path = require('path');
